74d38afb82d40480c5bb787c122808b2501e0280
AbouCloud <Abouzari6839@gmail.com>
2026-03-18 21:45:16 +0000

    home page updated share data with item-card

--- src/app/assets/home/home.ts
+++ src/app/assets/home/home.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { OnInit } from '@angular/core';
+import { OnInit, inject } from '@angular/core';
 import { Component } from '@angular/core';
 import { Navbar } from '../../component/navbar/navbar';
 import { Hero } from '../../component/hero/hero';
 import { ItemCard } from '../../component/item-card/item-card';
+import { Api } from '../../services/api/api';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -11,16 +12,41 @@ import { ItemCard } from '../../component/item-card/item-card';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
-  filters = ["All", "Hotel", "Hostel", "Appartment"]
-  filterValues: any[] = [{
-    type: "All",
+export class Home implements OnInit{
+  loading = true
+  api = inject(Api)
+  cardItems: any [] = []
+  // ------------filter ------
+  filters = ["All", "hotel", "hostel", "asppartment"]
+  filterValues = {
+    type: "hotel",
     page: 1,
     limit: 50
-  }]
-  filterClicked(filter:string){
-    this.filterValues = [{...this.filterValues[0], type: filter}]
-    console.log("filter clicked", this.filterValues)
   }
-  
+  filterClicked(filter:string){
+    this.filterValues.type = filter,
+    this.filterValues.page = 1
+    console.log("filter clicked", this.filterValues)
+    this.getAllRoom(this.filterValues)
+  }
+
+   ngOnInit(): void {
+    this.getAllRoom(this.filterValues)
+  }
+
+  getAllRoom(filterValues:any) {
+    this.loading = true
+    this.api.getProduct(filterValues).subscribe({
+      next: (resp: any) => {
+        this.cardItems = resp || [];
+        this.loading = false;
+        console.log("all the available room:", resp)
+      },
+      error: (err: any) => {
+        console.log("getting Room Failed :", err)
+        this.cardItems = [];
+        this.loading = false;
+      },
+    })
+  }
 }
