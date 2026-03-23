0c835167419955be0a993b6e0acdf8f5094afead
AbouCloud <Abouzari6839@gmail.com>
2026-03-23 22:28:50 +0000

    sign-in and sign-up page completed

--- src/app/assets/signIn-Up/sign-in/sign-in.ts
+++ src/app/assets/signIn-Up/sign-in/sign-in.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { Api } from '../../../services/api/api';
 import { HttpClient } from '@angular/common/http';
@@ -12,7 +12,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './sign-in.css',
 })
 export class SignIn implements OnInit {
-constructor(private http:HttpClient, private route:Router){}
+constructor(private http:HttpClient, private route:Router, private cdr:  ChangeDetectorRef){}
 api = inject(Api)
 message:any
 error:any
@@ -31,12 +31,15 @@ User= {
         this.message = resp.message
          this.error = {}; 
          setTimeout(() => {
-          this.route.navigate(['/home'])
+          this.route.navigate(['/'])
          }, 1800);
+         this.api.getUser()
+         this.cdr.detectChanges()
       },
       error: (err:any) => {
         this.error = err.message
         console.log("Failed to create User", err)
+         this.cdr.detectChanges()
       }
     })
   }
