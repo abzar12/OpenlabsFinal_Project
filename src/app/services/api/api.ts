51e3482eb6f9654727d05e9046faccb50e6864e8
AbouCloud <Abouzari6839@gmail.com>
2026-03-23 22:31:55 +0000

    items-card page updated

--- src/app/services/api/api.ts
+++ src/app/services/api/api.ts
@@ -1,14 +1,14 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, OnInit, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root',
 })
-export class Api {
+export class Api  {
   baseURL = "http://localhost:8000/sissoko-room/api"
   http = inject(HttpClient)
-
   getProduct(filterValues:any){
     return this.http.get(`${this.baseURL}/getproducts`, {
       params: filterValues
@@ -20,7 +20,18 @@ export class Api {
   createUser(forms:any){
     return this.http.post(`${this.baseURL}/customer/signup`, forms)
   }
-  logInUser(forms:any){
-    return this.http.post(`${this.baseURL}/customer/login`, forms)
+  logInUser(data:any){
+    return this.http.post(`${this.baseURL}/customer/login`, data, {
+      withCredentials:true
+    })
+  }
+  logOut(): Observable<any>{
+    return this.http.post(`${this.baseURL}/logout`, {}, {withCredentials:true})
+  }
+  getUser(){
+    return this.http.get(`${this.baseURL}/me`, {withCredentials: true})
+  }
+  refreshToken(): Observable<any>{
+    return this.http.post(`${this.baseURL}/refreshtk`, {}, {withCredentials: true})
   }
 }
