78a4502ac8d4fc242d8a03ca71ec5eee31f18dbf
AbouCloud <Abouzari6839@gmail.com>
2026-03-23 22:29:49 +0000

    view product page updated working perfectelly

--- src/app/assets/view-product/view-product.ts
+++ src/app/assets/view-product/view-product.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, inject, ChangeDetectorRef, computed, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { environment } from '../../../environments/environment';
 import { Api } from '../../services/api/api';
+import { Card } from '../../services/card/card';
 import { Navbar } from '../../component/navbar/navbar';
 import { CommonModule } from '@angular/common';
+import { sign } from 'crypto';
 
 interface Products {
   id: number,
@@ -28,33 +30,42 @@ interface Products {
 })
 
 export class ViewProduct implements OnInit {
-  constructor(private cdr:  ChangeDetectorRef){}
+  constructor(private cdr: ChangeDetectorRef) { }
   imageBase = environment.IMAGE_PATH
-  api = inject(Api)
+  cardService = inject(Card)
+  api = inject(Api) // for the api services
   slug: any
   router = inject(ActivatedRoute)
   product: any = null;
-  // is_loading: boolean = false
-
+  is_loading: boolean = false
   ngOnInit(): void {
     this.viewProduct()
   }
   viewProduct() {
-    // this.is_loading = true
+    this.is_loading = true
     this.slug = this.router.snapshot.paramMap.get('slug')
     console.log("snapshot SLUG got:", this.slug)
     this.api.getProductBySlug(this.slug).subscribe({
       next: (resp: Products | any) => {
         this.product = resp
         console.log("this is response", resp)
-        // this.is_loading = false
+        this.is_loading = false
         this.cdr.detectChanges()
       },
       error: (err) => {
         console.log("Getting unique product Failed", err)
-        // this.is_loading = false
+        this.is_loading = false
         this.cdr.detectChanges()
       }
     })
+  }
+  reservedRoom() {
+    this.cardService.addToCard(this.product)
+  }
+  removeRoom(uuid: string) {
+    this.cardService.removeFromCard(uuid)
+  }
+  isAdded = (uuid:any) => {
+    return this.cardService.isAdded(uuid)
   }
 }
