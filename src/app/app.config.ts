31b47ac5e546247e39d7eb3943bb97af3aa4e31f
AbouCloud <Abouzari6839@gmail.com>
2026-03-25 23:32:28 +0000

    card page created it left the orders pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+// imported 
+import { provideAnimations } from "@angular/platform-browser/animations"
+import {provideToastr} from "ngx-toastr"
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
+    provideToastr(),
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(withFetch()),
     provideRouter(routes), provideClientHydration(withEventReplay()) 
