80f0bb2d9356aa99a2dc107de505d35bf308985b
AbouCloud <Abouzari6839@gmail.com>
2026-03-29 23:58:39 +0000

    navbar page updated

--- src/app/component/navbar/navbar.ts
+++ src/app/component/navbar/navbar.ts
@@ -4,7 +4,9 @@ import { Api } from '../../services/api/api';
 import { Card } from '../../services/card/card';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faUser,faCaretDown,faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faCaretDown, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { switchMap, catchError, tap, Observable } from 'rxjs';
+import { of, throwError } from 'rxjs';
 
 export interface UserProp {
   id: string,
@@ -23,43 +25,58 @@ export interface UserProp {
   styleUrl: './navbar.css',
 })
 export class Navbar implements OnInit {
-  constructor(private cdr: ChangeDetectorRef){}
+  private cdr = inject(ChangeDetectorRef);
   cardService = inject(Card)
   // cardLength =this.cardService ? this.cardService?.cardLength(): 1
-  Dropdown:boolean = false 
+  Dropdown: boolean = false
   faUser = faUser
-  faCaretDown=faCaretDown
-  cart =faShoppingCart
+  faCaretDown = faCaretDown
+  cart = faShoppingCart
   api = inject(Api)// refer to card service for getting the number of item in localstorage
-  user: UserProp | null = null
+  user$:Observable< UserProp | any>  = this.api.getUser().pipe(
+    catchError(() => this.api.refreshToken().pipe(
+      switchMap(() => this.api.getUser()),
+      catchError(() => {
+        this.logOut();
+        return of(null);
+      })
+    ))
+  )
   ngOnInit(): void {
-    this.getCurrentUser()
-    console.log("navbar rendered")
+    console.log("navbar rendered", this.user$)
+
   }
-  getCurrentUser() {
-    this.api.getUser().subscribe({
-      next: (resp: any) => {
-        this.user = resp
-        this.cdr.detectChanges()
-      },
-      error: () => {
-        this.api.refreshToken().subscribe({
-          next: () => {
-            this.getCurrentUser()
-            this.cdr.detectChanges()
-          },
-          error: () => {
-            this.api.logOut().subscribe(); // logout if refresh fails
-            this.cdr.detectChanges()
-          }
-        })
-      }
-    })
-  }
-  OnpenDropdown(){
+  // getCurrentUser(retry = true) {
+  //   this.api.getUser().pipe(
+  //     tap((resp: any) => {
+  //       this.user = resp
+  //       console.log(resp)
+  //       // this.cdr.detectChanges()
+  //     }),
+  //     catchError(err => {
+  //       if (retry) {
+  //         return this.api.refreshToken().pipe(
+  //           switchMap(() => this.getCurrentUser(false)),
+  //           catchError(() => {
+  //             console.log('Refresh Failed');
+  //             this.logOut();
+  //             return of(null);
+  //           })
+  //         );
+  //       } else {
+  //         this.logOut();
+  //       return of(null);
+  //       }
+  //     })
+  //   ).subscribe
+  // }
+  OnpenDropdown() {
     this.Dropdown = !this.Dropdown
   }
-  logOut(){
-    this.api.logOut()
+  logOut() {
+    this.api.logOut().subscribe({
+      next: () => console.log('User logged out '),
+      error: ()=> console.log('User logged out Failed'),
+    })
   }
 }
