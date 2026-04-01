1d4c9296ff928b61f632f2544248406584325505
AbouCloud <Abouzari6839@gmail.com>
2026-04-01 11:06:06 +0000

    Fix import path for environment file

--- src/app/assets/view-product/view-product.ts
+++ src/app/assets/view-product/view-product.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, inject, ChangeDetectorRef, computed, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.ts';
 import { Api } from '../../services/api/api';
 import { Card } from '../../services/card/card';
 import { Navbar } from '../../component/navbar/navbar';
